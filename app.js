96cbc4fa6cd8e3d7206f6432a0cccd869871fb0d
Raihan <lagbena@gmail.com>
2020-07-14 03:14:58 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+const bdParser = require('body-parser');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/Shop');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(bdParser.urlencoded());
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
+app.listen(3000);
+
+
+
