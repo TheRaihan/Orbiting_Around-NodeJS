48a8ec4f1ccbdd3c92aba8a69ec18b21bafc335f
Raihan Taher <rayhan.taher@gmail.com>
2020-07-15 02:16:45 +0600

    editted pug

--- routes/admin.js
+++ routes/admin.js
@@ -5,18 +5,16 @@ const router = express.Router();
 const products = [];
 
 // admin/add-product -- GET
-router.get('/add-product',(req,res,next) => {
-    // res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
-  });
-  
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', { PageTitle: 'Add Product' })
+});
 
-// admin/add-product -- POST
-router.post('/add-product',(req,res,next) => {
-    //   console.log(req.body);
-      products.push(req.body.title)
-      res.redirect('/'); 
-  });
+
+// admin/add-product -- POST 
+router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title })
+    res.redirect('/');
+});
 
 exports.routes = router;
 exports.products = products;
